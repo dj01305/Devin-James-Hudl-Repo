849a7d013e4dbb5cdb2125c361476083908abcb1
dj01305 <104515292+dj01305@users.noreply.github.com>
2022-04-27 14:50:47 -0400

    Add files via upload

---
+++ scrollFeature/ScrollForElement.java
@@ -0,0 +1,39 @@
+package scrollFeature;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import baseClass.HudlBaseDriver;
+import utilities.MyLogger;
+
+public class ScrollForElement extends HudlBaseDriver {
+	private WebDriver driver;
+	MyLogger oLogger = new MyLogger();
+
+	@FindBy(xpath = "//button[@id='logInWithOrganization']")
+	private WebElement logInWithOrganizationButton;
+
+	// Constructor to initialize webdriver, webdriverwait and page objects
+	public ScrollForElement(WebDriver driver, WebDriverWait wait) {
+
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+
+	// Method to scroll for the Log in with Organization button
+	public void scrollForlogInWithOrganizationButton() throws InterruptedException {
+
+		JavascriptExecutor je = (JavascriptExecutor) driver;
+		WebElement element = driver.findElement(By.xpath("(//button[@id='logInWithOrganization'])"));
+		je.executeScript("arguments[0].scrollIntoView(true);", element);
+		oLogger.info("Scrolled for log in with organization button");
+		Thread.sleep(1000);
+
+	}
+}
